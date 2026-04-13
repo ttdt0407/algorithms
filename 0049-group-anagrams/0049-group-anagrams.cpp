489c67cc4c9bd0ae8bdba0bcce68c12e439d5ba5
Ta Tran Dinh Tien <tatrandinhtien@gmail.com>
2026-04-13 18:19:10 +0700

    Time: 11 ms (91.15%), Space: 25.9 MB (55.35%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        unordered_map<string, vector<string>> mp;
+    
+        for (int i = 0; i < strs.size(); i++)
+        {
+            string key = strs[i];
+            sort(key.begin(), key.end());
+            mp[key].push_back(strs[i]);
+        }
+
+        vector<vector<string>> ans;
+        for (pair<string, vector<string>> const &x : mp)
+        {
+            ans.push_back(x.second);
+        }
+        
+        return ans;
+    }
+};
