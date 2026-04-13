5de9cdf43c9b6b674548585d9e15172020d70d8a
Ta Tran Dinh Tien <tatrandinhtien@gmail.com>
2026-04-13 18:17:50 +0700

    Time: 0 ms (100%), Space: 8.8 MB (42.98%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.c
@@ -0,0 +1,36 @@
+char findTheDifference(char* s, char* t) {
+    
+    if (s == NULL || t == NULL)
+    {
+        return -1;
+    }
+
+
+    int A[26];
+    int lens = strlen(s);
+    int lent = lens + 1;
+
+    char res = 0;
+
+    for (int i = 0; i < 26; i++)
+    {
+        A[i] = 0;
+    }
+
+    for (int i = 0; i < lens; i++)
+    {
+        int idx1 = s[i] - 97;
+
+        A[idx1]++;
+    }
+
+    for (int i = 0; i < lent; i++)
+    {
+        int idx2 = t[i] - 97;
+
+        if (--A[idx2] < 0)
+            res = t[i];
+    }
+
+    return res;
+}
