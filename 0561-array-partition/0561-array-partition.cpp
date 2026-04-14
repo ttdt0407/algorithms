9e54a9853c4eabc493d48c904b2e188e14b393b7
Ta Tran Dinh Tien <tatrandinhtien@gmail.com>
2026-04-14 09:35:02 +0700

    Time: 16 ms (23.06%), Space: 32 MB (84.72%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+
+        long long ans = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (i % 2 == 0)
+            {
+                ans += nums[i];
+            }
+        }
+
+        return ans;
+    }
+};
