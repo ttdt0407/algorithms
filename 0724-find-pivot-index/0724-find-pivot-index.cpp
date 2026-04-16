3d49c664a23bea04cc72914d7cc3d853ba9ac282
Ta Tran Dinh Tien <tatrandinhtien@gmail.com>
2026-04-16 22:03:10 +0700

    Time: 1 ms (25.78%), Space: 36.6 MB (16.34%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        vector<long long> pre;
+        int n = nums.size();
+        pre.resize(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i == 0) pre[i] = nums[i];
+            else pre[i] = pre[i-1] + nums[i];
+        }
+
+        int ans = -1;
+        for (int i = 0; i < n; i++)
+        {
+            
+            long long left_sum = (i == 0) ? 0 : pre[i-1];
+            long long right_sum = (i == n - 1) ? 0 : (pre[n-1] - pre[i]);
+            if (left_sum == right_sum)
+                return i;
+        }
+        
+        return ans;
+    }
+};
