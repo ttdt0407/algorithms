a621249b8e3575bdaeadee42985b3ab2fefae104
Ta Tran Dinh Tien <tatrandinhtien@gmail.com>
2026-04-17 10:05:07 +0700

    Time: 0 ms (100%), Space: 72.1 MB (73.99%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector<int> ans (n + 1, 0);
+
+        int s = bookings.size();
+
+        for (int i = 0; i < s; i++)
+        {
+            ans[bookings[i][0] - 1] += bookings[i][2];
+            ans[bookings[i][1]] -= bookings[i][2];
+        }
+
+        long long flags = 0;
+        for (int i  = 0; i < n; i++)
+        {
+            flags += ans[i];
+            ans[i] = flags;
+        }
+        ans.pop_back();
+        return ans;
+    }
+};
