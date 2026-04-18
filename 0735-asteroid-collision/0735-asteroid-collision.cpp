62c331abf23513008985882331c1d8c54ed8ecda
Ta Tran Dinh Tien <tatrandinhtien@gmail.com>
2026-04-18 21:37:23 +0700

    Time: 0 ms (100%), Space: 21.5 MB (96.81%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,46 +1,36 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
+        
         vector<int> ans;
 
-        for (int x : asteroids)
+        for (int x :  asteroids)
         {
-            if (ans.empty())
+            int flag = 0;
+            while(x < 0 && !ans.empty() && ans.back() > 0)
+            {
+                if (abs(x) > ans.back())
+                {
+                    ans.pop_back();
+                }
+                else if (abs(x) == ans.back())
+                {
+                    ans.pop_back();
+                    flag = 1;
+                    break;
+                }
+                else {
+                    flag = 1;
+                    break;
+                }
+            }
+
+            if (!flag)
             {
                 ans.push_back(x);
-            }
-            else if (x > 0)
-            {
-                ans.push_back(x);
-            }
-            else
-            {
-                int flag = 0;
-                while(!ans.empty() && ans.back() > 0)
-                {
-                    if (abs(x) > ans.back())
-                    {
-                        ans.pop_back();
-                    }
-                    else if (abs(x) == ans.back())
-                    {
-                        ans.pop_back();
-                        flag = 1;
-                        break;
-                    }
-                    else
-                    {
-                        flag = 1;
-                        break;
-                    }
-                }
-                if (!flag)
-                {
-                    ans.push_back(x);
-                }
             }
         }
-
+        
         return ans;
     }
 };
