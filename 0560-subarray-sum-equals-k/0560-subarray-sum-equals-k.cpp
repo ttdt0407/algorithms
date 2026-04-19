4964cca1cd462602d47262dad19f26d08260856a
Ta Tran Dinh Tien <tatrandinhtien@gmail.com>
2026-04-19 22:42:18 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
+        
         int res = 0;
-
         int size = nums.size();
-
-        vector<long long> pre(size+1, 0);
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int pre[size + 1];
+        pre[0] = 0;
         map<int, int> mp;
-
         mp[0] = 1;
         for (int i = 1; i <= size; i++)
         {
