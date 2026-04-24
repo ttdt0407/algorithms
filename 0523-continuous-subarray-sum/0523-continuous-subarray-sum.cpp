542ed214f9763aeeaca0f72ea37a2c2ce4c3bd45
Ta Tran Dinh Tien <tatrandinhtien@gmail.com>
2026-04-24 23:11:43 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -4,14 +4,11 @@ public:
         unordered_map<int, int> mp;
 
         long long sum = 0;
+        mp[0] = -1;
         for (int i = 0; i < nums.size(); i++)
         {
             sum += nums[i];
             int temp = sum % k;
-            if (temp == 0 && i >= 1)
-            {
-                return true;
-            }
             if (mp.count(temp))
             {
                 if (i - mp[temp] >= 2)
